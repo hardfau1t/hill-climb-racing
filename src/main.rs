4ba9752f59a9327ec3bfaeed10fd9ffaf9369eb6
hardfau1t <hardfau1t@proton.me>
2025-04-27 19:51:37 +0530

    feat(main): basic player and earth drawing done

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,107 @@
 use bevy::{
-    color::palettes::css::{BLUE, GRAY, RED},
+    color::palettes::css::{GREEN, RED},
+    math::vec3,
     prelude::*,
 };
 
-#[derive(Component)]
-struct Player;
+const WINDOW_WIDTH: f32 = 1200.;
+const WINDOW_HEIGHT: f32 = 800.;
+
+const GROUND_Y: f32 = -((WINDOW_HEIGHT / 2.) / 2.);
+const CAR_DEFAULT_POSITION: Vec3 = vec3(
+    -((3.0 * WINDOW_WIDTH) / (4.0 * 2.0) - (CAR_WIDTH / 2.0)),
+    GROUND_Y,
+    0.,
+);
+const EARTH_WIDTH: f32 = WINDOW_WIDTH;
+const EARTH_HEIGHT: f32 = WINDOW_HEIGHT / 2.0 + GROUND_Y;
+const EARTH_POSITION: Vec3 = vec3(0., (-WINDOW_HEIGHT / 2.0) + (EARTH_HEIGHT / 2.0), 0.);
+const CAR_HEIGHT: f32 = WINDOW_HEIGHT / 20.;
+const CAR_WIDTH: f32 = WINDOW_WIDTH / 12.;
+const DEFAULT_VELOCITY_INCREASE: f32 = WINDOW_HEIGHT;
+const GRAVITY_PULL_FACTOR: f32 = DEFAULT_VELOCITY_INCREASE / 40.;
 
 #[derive(Component)]
-struct Enemy;
+struct Car;
 
-const PLAYER_SPEED: f32 = 200.0;
+#[derive(Component)]
+struct Earth;
 
-fn main() {
-    App::new()
-        .insert_resource(ClearColor(GRAY.into()))
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, (player_movement, exit_game))
-        .run();
-}
-
-fn exit_game(keyboard_input: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
-    if keyboard_input.pressed(KeyCode::Escape) {
-        exit.send(AppExit::Success);
-    }
-}
+#[derive(Component)]
+struct Velocity(f32);
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2d::default());
+    // setup camera
+    commands.spawn(Camera2d);
 
+    // spawn car
     commands.spawn((
-        Mesh2d(meshes.add(Mesh::from(Circle::new(50.))).into()),
-        MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(BLUE)))),
-        Transform::from_translation(Vec3::new(0., 0., 0.)),
-        Player,
+        Mesh2d(meshes.add(Rectangle::new(CAR_WIDTH, CAR_HEIGHT))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(RED))),
+        Transform {
+            translation: CAR_DEFAULT_POSITION,
+            ..default()
+        },
+        Velocity(0.),
+        Car,
     ));
+
+    // spawn earth
     commands.spawn((
-        Mesh2d(meshes.add(Mesh::from(Circle::new(50.))).into()),
-        MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(RED)))),
-        Transform::from_translation(Vec3::new(-100., 0., 0.)),
-        Enemy,
+        Mesh2d(meshes.add(Rectangle::new(
+            EARTH_WIDTH,
+            EARTH_HEIGHT,
+        ))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(GREEN))),
+        Transform {
+            translation: EARTH_POSITION,
+            ..default()
+        },
+        Earth,
     ));
 }
 
-fn player_movement(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut player: Query<&mut Transform, (With<Player>, Without<Enemy>)>,
-    mut enemy: Query<&mut Transform, (With<Enemy>, Without<Player>)>,
-    time: Res<Time>,
+fn move_car(
+    car_query: Single<(&mut Transform, &mut Velocity), With<Car>>,
+    keyboard_inputs: Res<ButtonInput<KeyCode>>,
+    timer: Res<Time>,
 ) {
-    let mut player_transform = player.single_mut();
-    let mut direction = Vec3::ZERO;
-
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
-        direction.y += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::ArrowDown) {
-        direction.y -= 1.0;
+    let (mut car_pos, mut velocity) = car_query.into_inner();
+    for key in keyboard_inputs.get_just_pressed() {
+        match key {
+            KeyCode::Space => {
+                if car_pos.translation.y == GROUND_Y + CAR_HEIGHT / 2. {
+                    velocity.0 = DEFAULT_VELOCITY_INCREASE
+                }
+            }
+            _ => debug!("ignoring keypress: {key:?}"),
+        }
     }
 
-    if direction.length() > 0.0 {
-        direction = direction.normalize();
-    }
+    velocity.0 -= GRAVITY_PULL_FACTOR;
 
-    player_transform.translation += direction * PLAYER_SPEED * time.delta_secs();
+    car_pos.translation.y += timer.delta_secs() * velocity.0;
+    if car_pos.translation.y < GROUND_Y + CAR_HEIGHT / 2.  {
+        velocity.0 = 0.;
+        car_pos.translation.y = GROUND_Y + CAR_HEIGHT / 2.0;
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "hill climb racing".to_string(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_car)
+        .run();
 }
